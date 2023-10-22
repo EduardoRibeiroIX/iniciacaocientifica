8ba117814c49dde0f6b40bb69efe8951308976ec
EduardoRibeiroIX <eduardo.ribeiro@itec.ufpa.br>
2023-10-22 17:34:05 -0300

    função nas classes

--- PFL-Non-IID/system/flcore/servers/serveravg.py
+++ PFL-Non-IID/system/flcore/servers/serveravg.py
@@ -56,17 +56,17 @@ class FedAvg(Server):
                     break
                     
                 df_clientes = self.csv_clients(self.users)
-                df_cluster_clientes = self.cluster_FuzzyCMeans(df_clientes, k)
+                df_cluster_clientes = self.cluster_MiniBatchKMeans(df_clientes, k)
 
             else:
                 clientes_cluster = self.clientes_cluster_random(df_cluster_clientes, self.obj_clients)
                 self.selected_clients = list(clientes_cluster[1].values())
                 df_clientes = self.csv_clients(self.users)
-                df = self.cluster_FuzzyCMeans(df_clientes, k)
+                df = self.cluster_MiniBatchKMeans(df_clientes, k)
                 self.users = []
                 self.treinamento(i, self.selected_clients)
                 df = self.updated_data(df, clientes_cluster[0], self.users)
-                df_cluster_clientes = self.cluster_FuzzyCMeans(df, k)
+                df_cluster_clientes = self.cluster_MiniBatchKMeans(df, k)
                 self.users = [df_cluster_clientes['Media_clients'].tolist()]
 
                 if self.auto_break and self.check_done(acc_lss=[self.rs_test_acc], top_cnt=self.top_cnt):
